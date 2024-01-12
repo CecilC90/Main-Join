fd9553f1225915c0a6196b9db470615db29e5121
julian <julian.weishaar@gmx.de>
2024-01-12 15:42:05 +0100

    update add task page

--- js/add_task.js
+++ js/add_task.js
@@ -1,8 +1,20 @@
+let selectedPrio = 'medium';
+
 async function init() {
   await includesHTML();
   showSelectedButton('addTaskButton');
+  setPrioButton('medium')
 }
 
 function addTask(){
     console.log('add Task');
+}
+
+function setPrioButton(prio){
+  let selectedOldPrioID = 'prioButton' + selectedPrio.charAt(0).toUpperCase() + selectedPrio.slice(1);
+  let selectedPrioID = 'prioButton' + prio.charAt(0).toUpperCase() + prio.slice(1);
+  document.getElementById(selectedOldPrioID).classList.replace(selectedOldPrioID, 'prioButton');
+  document.getElementById(selectedPrioID).classList.replace('prioButton', selectedPrioID);
+  selectedPrio = prio;
+  console.log(selectedPrio);
 }
