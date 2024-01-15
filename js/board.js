ca544e9148482fcdf83095dd54704fc8b5d6bc63
SilasSchiel <silas-schiel@gmx.de>
2024-01-15 21:23:47 +0100

    change detailView funtion and add renderTodos function

--- js/board.js
+++ js/board.js
@@ -20,7 +20,14 @@ let todos = [
         title: 'Todo2',
         todoCategory: 'Test Category',
         description: 'Ths is the second Todo.',
-        category: 'open'
+        category: 'done'
+    },
+    {
+        id: 2,
+        title: 'Todo3',
+        todoCategory: 'Test Category',
+        description: 'Ths is the third Todo.',
+        category: 'progress'
     }
 ]
 
@@ -34,80 +41,48 @@ function init() {
 // Styles width JavaScript
 
 function renderHTML() {
-    renderCategoryOpen();
-    renderCategoryProgress();
-    renderCategorFeedback();
-    renderCategoryDone();
+    renderTodos();
 }
 
-function renderCategoryOpen() {
-    let open = todos.filter(t => t['category'] == 'open');
+function showDetailView(index) {
+   let detailViewContainer = document.getElementById('show-detail-todo');
+   detailViewContainer.style.display = 'flex';
+
+   detailViewContainer.innerHTML += templateHTMLDetailView(index)
+
+}   
+
+function templateHTMLDetailView(index) { 
+    return `
+        <div id="detail-todo-content">${todos[index]['title']}</div>
+    `;
+}
+
+function renderTodos() {
     let boardContentTodo = document.getElementById('board-content-todo');
-    let openContent = document.getElementById('content-todo');
-    if(!open.length == 0) {
-        boardContentTodo.classList.remove('board-content');
-        boardContentTodo.innerHTML = '';
-    } else {
-        boardContentTodo.classList.add('board-content');
-        boardContentTodo.innerHTML = 'No tasks To Do';
-    }
-    openContent.innerHTML = '';
-    for(let i = 0; i < open.length; i++) {
-        openContent.innerHTML += templateHTMLTodoContainer(open[i], i);
-    }
-}
-
-function renderCategoryProgress() {
-    let progress = todos.filter(t => t['category'] == 'progress');
     let boardContentProgress = document.getElementById('board-content-progress');
-    let progressContent = document.getElementById('content-progress');
-    document.getElementById('drag-progress').style.display = "none";
-    if(!progress.length == 0) {
-        boardContentProgress.classList.remove('board-content');
-        boardContentProgress.innerHTML = '';
-    } else {
-        boardContentProgress.classList.add('board-content');
-        boardContentProgress.innerHTML = 'No tasks in progress';
-    }
-    progressContent.innerHTML = '';
-    for(let i = 0; i < progress.length; i++) {
-        progressContent.innerHTML += templateHTMLTodoContainer(progress[i], i);
-    }
-}
-
-function renderCategorFeedback() {
-    let feedback = todos.filter(t => t['category'] == 'feedback');
     let boardContentFeedback = document.getElementById('board-content-feedback');
-    let feedbackContent = document.getElementById('content-feedback');
-    document.getElementById('drag-feedback').style.display = "none";
-    if(!feedback.length == 0) {
-        boardContentFeedback.classList.remove('board-content');
-        boardContentFeedback.innerHTML = '';
-    } else {
-        boardContentFeedback.classList.add('board-content');
-        boardContentFeedback.innerHTML = 'No tasks await for feedback';
-    }
-    feedbackContent.innerHTML = '';
-    for(let i = 0; i < feedback.length; i++) {
-        feedbackContent.innerHTML += templateHTMLTodoContainer(feedback[i], i);
-    }
-}
-
-function renderCategoryDone() {
-    let done = todos.filter(t => t['category'] == 'done');
     let boardContentDone = document.getElementById('board-content-done');
-    let doneContent = document.getElementById('content-done');
-    document.getElementById('drag-done').style.display = "none";
-    if(!done.length == 0) {
-        boardContentDone.classList.remove('board-content');
-        boardContentDone.innerHTML = '';
-    } else {
-        boardContentDone.classList.add('board-content');
-        boardContentDone.innerHTML = 'No tasks are done';
-    }
-    doneContent.innerHTML = '';
-    for(let i = 0; i < done.length; i++) {
-        doneContent.innerHTML += templateHTMLTodoContainer(done[i], i);
+
+    boardContentTodo.innerHTML = '';
+    boardContentProgress.innerHTML = '';
+    boardContentFeedback.innerHTML = '';
+    boardContentDone.innerHTML = '';
+
+    for(let i = 0; i < todos.length; i++) {
+        const todo = todos[i];
+        if(todo['category'] == 'open') {
+            boardContentTodo.innerHTML += templateHTMLTodoContainer(todo, i);
+        }
+        if(todo['category'] == 'progress') {
+            boardContentProgress.innerHTML += templateHTMLTodoContainer(todo, i);
+        }
+        if(todo['category'] == 'feedback') {
+            boardContentFeedback.innerHTML += templateHTMLTodoContainer(todo, i);
+        }
+        if(todo['category'] == 'done') {
+            boardContentDone.innerHTML += templateHTMLTodoContainer(todo, i);
+        }
     }
 }
 
@@ -135,58 +110,6 @@ function closeAddTask() {
     showAddTodoContainer.style.display = "none";
 }
 
-function showDetailView(index) {
-    renderCategoryOpenDetailView(index);
-    renderCategoryProgressDetailView(index);
-    renderCategoryFeedbackDetailView(index);
-    renderCategoryDoneDetailView(index);
-}
-
-function renderCategoryOpenDetailView(index) {
-    let filteredTodos = todos.filter(t => t['category'] == 'open');
-    let element = filteredTodos[index];
-    let showDetailTodoContainer = document.getElementById('show-detail-todo');
-    showDetailTodoContainer.style.display = "flex";
-    showDetailTodoContainer.innerHTML = templateHTMLDetailView(element);
-}
-
-function renderCategoryProgressDetailView(index) {
-    let filteredTodos = todos.filter(t => t['category'] == 'feedback');
-    let element = filteredTodos[index];
-    let showDetailTodoContainer = document.getElementById('show-detail-todo');
-    showDetailTodoContainer.style.display = "flex";
-    showDetailTodoContainer.innerHTML = templateHTMLDetailView(element);
-}
-
-function renderCategoryFeedbackDetailView(index) {
-    let filteredTodos = todos.filter(t => t['category'] == 'feedback');
-    let element = filteredTodos[index];
-    let showDetailTodoContainer = document.getElementById('show-detail-todo');
-    showDetailTodoContainer.style.display = "flex";
-    showDetailTodoContainer.innerHTML = templateHTMLDetailView(element);
-}
-
-function renderCategoryDoneDetailView(index) {
-    let filteredTodos = todos.filter(t => t['category'] == 'done');
-    let element = filteredTodos[index];
-    let showDetailTodoContainer = document.getElementById('show-detail-todo');
-    showDetailTodoContainer.style.display = "flex";
-    showDetailTodoContainer.innerHTML = templateHTMLDetailView(element);
-}
-
-function templateHTMLDetailView(element) { 
-        return `
-            <div id="detail-todo-content">
-                <span id="category-span">${element.todoCategory}</span>
-                <h2>${element.title}</h2>
-                <p>${element.description}</p>
-                Due Date: <p>${element.dueDate}</p>
-                Priority: <p>${element.priority}</p>
-                <h3>Assigned to:</h3> Currently empty
-            </div>
-        `;
-}
-
 function filterTodos() {
     let search = document.getElementById('search').value;
     search.toLowerCase();
@@ -202,3 +125,77 @@ function filterTodos() {
     }
 }
 
+
+
+
+// Code der demnächst gelöscht wird.
+function renderCategoryOpen() {
+    let open = todos.filter(t => t['category'] == 'open');
+    let boardContentTodo = document.getElementById('board-content-todo');
+    let openContent = document.getElementById('content-todo');
+    if(!open.length == 0) {
+        boardContentTodo.classList.remove('board-content');
+        boardContentTodo.innerHTML = '';
+    } else {
+        boardContentTodo.classList.add('board-content');
+        boardContentTodo.innerHTML = 'No tasks To Do';
+    }
+    openContent.innerHTML = '';
+    for(let i = 0; i < open.length; i++) {
+        const element = done[i];
+        openContent.innerHTML += templateHTMLTodoContainer(open[i], i);
+    }
+}
+
+function renderCategoryProgress() {
+    let progress = todos.filter(t => t['category'] == 'progress');
+    let boardContentProgress = document.getElementById('board-content-progress');
+    let progressContent = document.getElementById('content-progress');
+    document.getElementById('drag-progress').style.display = "none";
+    if(!progress.length == 0) {
+        boardContentProgress.classList.remove('board-content');
+        boardContentProgress.innerHTML = '';
+    } else {
+        boardContentProgress.classList.add('board-content');
+        boardContentProgress.innerHTML = 'No tasks in progress';
+    }
+    progressContent.innerHTML = '';
+    for(let i = 0; i < progress.length; i++) {
+        const element = done[i];
+        progressContent.innerHTML += templateHTMLTodoContainer(progress[i], i);
+    }
+}
+
+function renderCategorFeedback() {
+    let feedback = todos.filter(t => t['category'] == 'feedback');
+    let boardContentFeedback = document.getElementById('board-content-feedback');
+    let feedbackContent = document.getElementById('content-feedback');
+    document.getElementById('drag-feedback').style.display = "none";
+    if(!feedback.length == 0) {
+        boardContentFeedback.classList.remove('board-content');
+        boardContentFeedback.innerHTML = '';
+    } else {
+        boardContentFeedback.classList.add('board-content');
+        boardContentFeedback.innerHTML = 'No tasks await for feedback';
+    }
+    feedbackContent.innerHTML = '';
+    for(let i = 0; i < feedback.length; i++) {
+        const element = done[i];
+        feedbackContent.innerHTML += templateHTMLTodoContainer(feedback[i], );
+    }
+}
+
+function renderCategoryDone() {
+    let done = todos.filter(t => t['category'] == 'done');
+    let boardContentDone = document.getElementById('board-content-done');
+    let doneContent = document.getElementById('content-done');
+    document.getElementById('drag-done').style.display = "none";
+    if(!done.length == 0) {
+        boardContentDone.classList.remove('board-content');
+        boardContentDone.innerHTML = '';
+    } else {
+        boardContentDone.classList.add('board-content');
+        boardContentDone.innerHTML = 'No tasks are done';
+    }
+    doneContent.innerHTML = '';
+}
