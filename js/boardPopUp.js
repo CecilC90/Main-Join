be9c138db7b584043d3ffc5e2c27d3cee19e59b3
SilasSchiel <silas-schiel@gmx.de>
2024-02-09 15:27:49 +0100

    JS files organized

--- js/boardPopUp.js
+++ js/boardPopUp.js
@@ -229,6 +229,201 @@ function showAddTask() {
     mobileAddTask();
 }
 
+function renderAssingnedToDropdownListEditview() {
+    let content = document.getElementById("dropdownContentAssignedToEditview");
+    content.innerHTML = "";
+    for (let i = 0; i < contacts.length; i++) {
+      let firstAndSecondLetter = getFirstAndSecondLetterEditview(i);
+      content.innerHTML += renderAssingnedToDropdownListHTMLEditview(i, firstAndSecondLetter);
+      showSelectedDropdownContactEditview(i);
+    }
+  }
+  
+  function filterAssingnedToDropdownListEditview(){
+    let contactInput = document.getElementById('contactInput').value;
+    contactInput = contactInput.toLowerCase();
+    let content = document.getElementById("dropdownContentAssignedToEditview");
+    content.innerHTML = '';
+    for (let i = 0; i < contacts.length; i++) {
+      if(contacts[i]['name'].toLowerCase().includes(contactInput)){
+        let firstAndSecondLetter = getFirstAndSecondLetterEditview(i);
+        content.innerHTML += renderAssingnedToDropdownListHTML(i, firstAndSecondLetter);
+        showSelectedDropdownContactEditview(i);
+        dropdownContentAssignedToEditview.style.display = "flex";
+        toggleDropdownIconEditview("assignedToDropdownIconEditview", "flex");
+      }
+    }
+    if(contactInput.length == 0){
+      renderAssingnedToDropdownListEditview();
+      dropdownContentAssignedToEditview.style.display = "none";
+      toggleDropdownIconEditview("assignedToDropdownIconEditview", "none");
+    }
+  }
+  
+  function getFirstAndSecondLetterEditview(i) {
+    let name = contacts[i]["name"];
+    let splitName = name.split(" ");
+    let firstLetter = splitName[0].trim().charAt(0).toUpperCase();
+    let secondLetter = splitName[1] ? splitName[1].trim().charAt(0).toUpperCase() : "";
+    let result = firstLetter + secondLetter;
+    return result;
+  }
+  
+  function setContactSelectedEditview(i) {
+    if (contacts[i]["selected"]) {
+      contacts[i]["selected"] = false;
+      showSelectedDropdownContactEditview(i);
+    } else {
+      contacts[i]["selected"] = true;
+      showSelectedDropdownContactEditview(i);
+    }
+    renderSelectedContactsIconsEditview();
+  }
+  
+  function showSelectedDropdownContactEditview(i) {
+    let dropdownContact = document.getElementById("dropdownContactEditview" + i);
+    let dropdownContactImg = document.getElementById("dropdownContactImgEditview" + i);
+    if (contacts[i]["selected"]) {
+      dropdownContact.classList.replace("dropdownContacts", "dropdownContactsSelected");
+      dropdownContactImg.src = "/assets/img/checkbox_checked_white.svg";
+    } else {
+      dropdownContact.classList.replace("dropdownContactsSelected", "dropdownContacts");
+      dropdownContactImg.src = "/assets/img/checkbox_unchecked.svg";
+    }
+  }
+  
+  function renderSelectedContactsIconsEditview() {
+    let content = document.getElementById("showSelectedDropdownContactEditview");
+    content.innerHTML = "";
+    for (let i = 0; i < contacts.length; i++) {
+      if (contacts[i]["selected"]) {
+        content.innerHTML += /* html */ `
+          <div class="contactsIcon">${getFirstAndSecondLetter(i)}</div>
+        `;
+      }
+    }
+  }
+  
+  function contactDropdown() {
+      // Toggle dropdown visibility
+        dropdownContentAssignedToEditview.style.display = dropdownContentAssignedToEditview.style.display === "flex" ? "none" : "flex";
+        let dispayStatus = dropdownContentAssignedToEditview.style.display;
+        toggleDropdownIconEditview("assignedToDropdownIconEditview", dispayStatus);
+      };
+  
+  function toggleDropdownIconEditview(id, dispayStatus) {
+    if (dispayStatus == "flex") {
+        document.getElementById(id).src = "/assets/img/arrow_drop_down_up.svg";
+    } else {
+        document.getElementById(id).src = "/assets/img/arrow_drop_down.svg";
+    }
+  }
+      
+  function renderAssingnedToDropdownListHTMLEditview(i, firstAndSecondLetter) {
+    return /*html */ `
+      <div class="dropdownContacts" id="dropdownContactEditview${i}" onclick="setContactSelectedEditview(${i})">
+        <div class="dropdownContactNameConatiner">
+          <div class="contactsIcon">${firstAndSecondLetter}</div>
+          <p>${contacts[i]['name']}</p>
+        </div>
+        <img id="dropdownContactImgEditview${i}" src="/assets/img/checkbox_unchecked.svg" alt="checkbox_unchecked">
+      </div>
+    `;
+  }
+
+  function renderSubtasksEditview(index) {
+    let subtasksList = document.getElementById("subtasksListEditview");
+    subtasksList.innerHTML = "";
+    for (let i = 0; i < todos[index].subtask.length; i++) {
+      let subtasks = todos[index].subtask[i].title;  
+      subtasksList.innerHTML += renderSubtasksHTMLEditview(i, subtasks, index);
+    }
+  }
+
+  function renderSubtasksHTMLEditview(i, subtasks, index) {
+    return /* html */ `
+    <div id="subtaskEditview${i}" ondblclick="editSubtaskEditview(${i})">
+      <div class="subtask">
+        <div class="subtaskText">
+          <p>&bull;</p>
+          <P>${subtasks}</P>
+        </div>
+        <div class="subtaskMenu">
+          <img src="/assets/img/subtasks_edit_icon.svg" onclick="editSubtaskEditview(${i}, '${subtasks}', ${index})" alt="edit_icon">
+          <img src="/assets/img/subtasks_seperator.svg" alt="subtasks_seperator">
+          <img src="/assets/img/subtasks_delete_icon.svg" onclick="deleteSubtaskEditview(${i}, ${index})" alt="delete_icon">
+        </div>
+      </div>
+    </div>
+    `;
+  }
+  
+  function editSubtaskEditview(i, editSubtask, index) {
+    let content = document.getElementById("subtaskEditview" + i);
+    content.innerHTML = /* html */ `
+      <div class="subtaskEdit" id="subtaskEdit">
+        <input type="text" id="editSubtask${i}" value="${editSubtask}">
+        <div>
+          <img src="/assets/img/subtasks_delete_icon.svg" onclick="deleteSubtaskEditview(${i}, ${index})" alt="delete_icon">
+          <img src="/assets/img/subtasks_seperator.svg" alt="subtasks_seperator">
+          <img src="/assets/img/subtasks_done_icon.svg" onclick="editSubtaskDoneEditview(${i}, ${index})" alt="done_icon">
+        </div>
+      </div>
+    `;
+  }
+  
+  function editSubtaskDoneEditview(i, index) {
+    let content = document.getElementById("editSubtask" + i);
+    if(content.value !== '') {
+        todos[index].subtask[i].title = content.value;
+        renderSubtasksEditview(index);
+    }
+  }
+  
+  async function deleteSubtaskEditview(i, index) {
+    if(todos[index].subtask[i].subtaskDone == true) {
+      todos[index].counter--;
+    }
+    todos[index].subtask.splice(i, 1);
+    await setItem('allTasks', JSON.stringify(todos));
+    renderSubtasksEditview(index);
+  }
+
+  function showSubtasksDoneAndCancelEditview(index) {
+    let subtasksInput = document.getElementById("subtasksInputEditview");
+    let content = document.getElementById("subtasksInputMenuEditview");
+    if (subtasksInput.value.length != 0) {
+      content.innerHTML = /* html */ `
+        <img class="subtasksInputMenuimg" onclick="clearSubtaskInputFieldEditview()" src="/assets/img/subtasks_cancel_icon.svg" alt="cancel_icon">
+        <img src="/assets/img/subtasks_seperator.svg" alt="subtasks_seperator">
+        <img class="subtasksInputMenuimg" onclick="addSubtaskEditview(${index})" src="/assets/img/subtasks_done_icon.svg" alt="done_icon">
+     `;
+    } else {
+      content.innerHTML = '<img src="/assets/img/subtasks_add_icon.svg" alt="add_icon">';
+    }
+  }
+  
+  function clearSubtaskInputFieldEditview() {
+    let content = document.getElementById("subtasksInputEditview");
+    content.value = "";
+    showSubtasksDoneAndCancelEditview();
+    setBlueBorder('subtasksInput', 'subtaskField');
+  }
+
+  async function addSubtaskEditview(index) {
+    let subtasksInput = document.getElementById("subtasksInputEditview");
+    let addSubtask = todos[index].subtask;
+    addSubtask.push(
+        {
+            title: subtasksInput.value,
+            selected: false
+        }
+    );
+    
+    await setItem('allTasks', JSON.stringify(todos));
+    renderSubtasksEditview(index);
+}
+
 function closeAddTask() {
     let showAddTodoContainer = document.getElementById('show-add-todo');
     showAddTodoContainer.style.display = "none";
